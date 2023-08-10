0d5bf627c627208492b77c6d769ab704980e4ad0
emmanuel <eoziegbe2@gmail.com>
2023-08-10 12:34:23 +0100

    good

--- utils/images-remote.ts
+++ utils/images-remote.ts
@@ -14,86 +14,85 @@
 // I WILL EXPLAIN THIS IN THE NEXT CLASS
 
 // Also please note that the best way is to have these images in your public folder. So downloading dem will be a much better option
-
-// export const REMOTE_IMAGES = [
-// 	{
-// 		id: 0,
-// 		name: 'visa',
-// 		src: 'https://images.prismic.io/vivadotcom/ba4c8651-e79a-4a78-aafa-3964b98fd1e5_VISA_white+logo.svg',
-// 	},
-// 	{
-// 		id: 1,
-// 		name: 'mastercard',
-// 		src: 'https://images.prismic.io/vivadotcom/4c249be6-c021-4192-a26d-140abe7be84c_Mastercard.svg',
-// 	},
-// 	{
-// 		id: 2,
-// 		name: 'bancontact',
-// 		src: 'https://images.prismic.io/vivadotcom/4074bd93-e49c-4b0a-970d-0075e71440b1_Bancontact+%281%29.svg',
-// 	},
-// 	{
-// 		id: 3,
-// 		name: 'amex',
-// 		src: 'https://images.prismic.io/vivadotcom/76e326c6-1c4b-4017-b041-4de5a5cc904b_AMEX.svg',
-// 	},
-// 	{
-// 		id: 4,
-// 		name: 'klarna',
-// 		src: 'https://images.prismic.io/vivadotcom/c0e16744-d047-4e13-95be-4ce12d21fade_Klarna.svg',
-// 	},
-// 	{
-// 		id: 5,
-// 		name: 'paypal',
-// 		src: 'https://images.prismic.io/vivadotcom/f50f8aec-2c57-45f3-933b-478cccf5e055_PayPal_vertical+logo+white.svg',
-// 	},
-// 	{
-// 		id: 6,
-// 		name: 'diners',
-// 		src: 'https://images.prismic.io/vivadotcom/3a88eda9-8c56-40c4-9cb0-ebdfeb3295f2_Diners+%281%29.svg',
-// 	},
-// 	{
-// 		id: 7,
-// 		name: 'discover',
-// 		src: 'https://images.prismic.io/vivadotcom/28177fe7-f750-4d3e-8481-b48ca89fb67e_Discover+%281%29.svg',
-// 	},
-// 	{
-// 		id: 8,
-// 		name: 'samsungpay',
-// 		src: 'https://images.prismic.io/vivadotcom/4c5ed988-3ef2-47e4-a96a-1af10bc7845b_Samsung+Pay_vertical+logo+white.svg',
-// 	},
-// 	{
-// 		id: 9,
-// 		name: 'applepay',
-// 		src: 'https://images.prismic.io/vivadotcom/007044bd-d45b-4fb3-b0d1-3fa023f08060_Apple+Pay+%281%29.svg',
-// 	},
-// 	{
-// 		id: 9,
-// 		name: 'googlepay',
-// 		src: 'https://images.prismic.io/vivadotcom/10fa55d3-21e9-4709-8b3f-6ed35edba277_Google+Pay_logo+white.svg',
-// 	},
-// 	{
-// 		id: 10,
-// 		name: 'payconiq',
-// 		src: 'https://images.prismic.io/vivadotcom/78b9396a-cf4d-45ac-8512-86aeb243c5ca_Payconiq.svg',
-// 	},
-// 	{
-// 		id: 11,
-// 		name: 'cartesbancaires',
-// 		src: 'https://images.prismic.io/vivadotcom/afa639f4-128a-461a-b77b-2dee8b6f1a7d_Cartes+Bancaires.svg',
-// 	},
-// 	{
-// 		id: 12,
-// 		name: 'alipay',
-// 		src: 'https://images.prismic.io/vivadotcom/d3f67343-79db-4daf-a044-42a4f12a08b5_Alipay_logo+white.svg',
-// 	},
-// 	{
-// 		id: 13,
-// 		name: 'multibanco',
-// 		src: 'https://images.prismic.io/vivadotcom/0bc76356-1445-4851-8756-773a8913f860_MULTIBANCO+-+logo-white-01.svg',
-// 	},
-// 	{
-// 		id: 14,
-// 		name: 'mbway',
-// 		src: 'https://images.prismic.io/vivadotcom/6b13fd75-63e5-4593-81d3-d48877bdab28_mbway-logo-white-01.svg',
-// 	},
-// ];
+export const REMOTE_IMAGES = [
+	{
+		id: 0,
+		name: 'visa',
+		src: 'https://images.prismic.io/vivadotcom/ba4c8651-e79a-4a78-aafa-3964b98fd1e5_VISA_white+logo.svg',
+	},
+	{
+		id: 1,
+		name: 'mastercard',
+		src: 'https://images.prismic.io/vivadotcom/4c249be6-c021-4192-a26d-140abe7be84c_Mastercard.svg',
+	},
+	{
+		id: 2,
+		name: 'bancontact',
+		src: 'https://images.prismic.io/vivadotcom/4074bd93-e49c-4b0a-970d-0075e71440b1_Bancontact+%281%29.svg',
+	},
+	{
+		id: 3,
+		name: 'amex',
+		src: 'https://images.prismic.io/vivadotcom/76e326c6-1c4b-4017-b041-4de5a5cc904b_AMEX.svg',
+	},
+	{
+		id: 4,
+		name: 'klarna',
+		src: 'https://images.prismic.io/vivadotcom/c0e16744-d047-4e13-95be-4ce12d21fade_Klarna.svg',
+	},
+	{
+		id: 5,
+		name: 'paypal',
+		src: 'https://images.prismic.io/vivadotcom/f50f8aec-2c57-45f3-933b-478cccf5e055_PayPal_vertical+logo+white.svg',
+	},
+	{
+		id: 6,
+		name: 'diners',
+		src: 'https://images.prismic.io/vivadotcom/3a88eda9-8c56-40c4-9cb0-ebdfeb3295f2_Diners+%281%29.svg',
+	},
+	{
+		id: 7,
+		name: 'discover',
+		src: 'https://images.prismic.io/vivadotcom/28177fe7-f750-4d3e-8481-b48ca89fb67e_Discover+%281%29.svg',
+	},
+	{
+		id: 8,
+		name: 'samsungpay',
+		src: 'https://images.prismic.io/vivadotcom/4c5ed988-3ef2-47e4-a96a-1af10bc7845b_Samsung+Pay_vertical+logo+white.svg',
+	},
+	{
+		id: 9,
+		name: 'applepay',
+		src: 'https://images.prismic.io/vivadotcom/007044bd-d45b-4fb3-b0d1-3fa023f08060_Apple+Pay+%281%29.svg',
+	},
+	{
+		id: 9,
+		name: 'googlepay',
+		src: 'https://images.prismic.io/vivadotcom/10fa55d3-21e9-4709-8b3f-6ed35edba277_Google+Pay_logo+white.svg',
+	},
+	{
+		id: 10,
+		name: 'payconiq',
+		src: 'https://images.prismic.io/vivadotcom/78b9396a-cf4d-45ac-8512-86aeb243c5ca_Payconiq.svg',
+	},
+	{
+		id: 11,
+		name: 'cartesbancaires',
+		src: 'https://images.prismic.io/vivadotcom/afa639f4-128a-461a-b77b-2dee8b6f1a7d_Cartes+Bancaires.svg',
+	},
+	{
+		id: 12,
+		name: 'alipay',
+		src: 'https://images.prismic.io/vivadotcom/d3f67343-79db-4daf-a044-42a4f12a08b5_Alipay_logo+white.svg',
+	},
+	{
+		id: 13,
+		name: 'multibanco',
+		src: 'https://images.prismic.io/vivadotcom/0bc76356-1445-4851-8756-773a8913f860_MULTIBANCO+-+logo-white-01.svg',
+	},
+	{
+		id: 14,
+		name: 'mbway',
+		src: 'https://images.prismic.io/vivadotcom/6b13fd75-63e5-4593-81d3-d48877bdab28_mbway-logo-white-01.svg',
+	},
+];
